c46d66d6cc563b4377a6f2688f1cfbf7d0c4e943
slobadass <as8889937@gmail.com>
2025-03-25 18:56:28 +0300

    удалил лишнее

--- src/js/index.js
+++ src/js/index.js
@@ -26,16 +26,6 @@ const modalTitle = document.querySelector(".modal_title");
 const modalText = document.querySelector(".modal_text");
 const logoutBtn = document.querySelector('.logout-btn')
 
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC6NSFMcNA2F3SgoCodLUt53Rt6vlGmKXI",
-//   authDomain: "sf-app-database.firebaseapp.com",
-//   projectId: "sf-app-database",
-//   storageBucket: "sf-app-database.firebasestorage.app",
-//   messagingSenderId: "135936952195",
-//   appId: "1:135936952195:web:f6cdbf9d06143a532858b6",
-// };
-
 const firebaseConfig = {
   apiKey: "AIzaSyC6NSFMcNA2F3SgoCodLUt53Rt6vlGmKXI",
   authDomain: "sf-app-database.firebaseapp.com",
@@ -174,76 +164,3 @@ trainingsModal.addEventListener("click", (event) => {
 });
 
 logoutBtn.addEventListener('click', logout)
-
-
-
-
-// const firebaseConfig = {
-//     apiKey: "AIzaSyC6NSFMcNA2F3SgoCodLUt53Rt6vlGmKXI",
-//     authDomain: "sf-app-database.firebaseapp.com",
-//     projectId: "sf-app-database",
-//     storageBucket: "sf-app-database.firebasestorage.app",
-//     messagingSenderId: "135936952195",
-//     appId: "1:135936952195:web:f6cdbf9d06143a532858b6"
-// };
-
-
-// const app = initializeApp(firebaseConfig);
-// const auth = getAuth(app);
-
-
-
-// formRegistration.addEventListener('submit', async function (event) {
-//     event.preventDefault()
-//     const email = registerEmail.value;
-//     const password = registerPassword.value;
-//     try {
-//         const userCredential = await createUserWithEmailAndPassword(auth, email, password)
-//         console.log('Регистрация прошла успешно');
-//     } catch (error) {
-//         console.error('Ошибка регистрации: ', error)
-//     }
-// })
-
-// formAuth.addEventListener('submit', async function (event) {
-//     event.preventDefault()
-//     const email = authEmail.value;
-//     const password = authPassword.value;
-//     try {
-//         const userCredential = await signInWithEmailAndPassword(auth, email, password)
-//         console.log('Вход прошел успешно!');
-//         window.location.href = "index.html"
-//     } catch (error) {
-//         console.error('Ошибка входа: ', error)
-//     }
-// })
-
-
-
-// // click - обработчик событий
-// loginTab.addEventListener('click', () => {
-//     loginTab.classList.add('active');
-//     // active - изменение цвета эл-та
-//     registerTab.classList.remove('active');
-//     loginForm.style.display = 'block';
-//     registerForm.style.display = 'none';
-// });
-
-// registerTab.addEventListener('click', () => {
-//     registerTab.classList.add('active');
-//     loginTab.classList.remove('active');
-//     loginForm.style.display = 'none';
-//     registerForm.style.display = 'block';
-// });
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   const auth = getAuth();
-
-//   onAuthStateChanged(auth, (user) => {
-//     if(!user) {
-//       window.location.href = 'login.html'
-//     } else {
-//       console.log('Пользователь авторизован')
-//     }
-//   })
-// })
